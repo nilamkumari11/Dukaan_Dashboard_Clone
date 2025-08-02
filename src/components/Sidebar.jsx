9e4a1429f0acdda3c617fb4a8131e24c2f38ef76
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-08-02 23:55:54 +0530

    Update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ export const Sidebar = () => {
                 </svg>
                 <div className="flex flex-col ml-1 text-stone-50">
                     <h2 className="text-md font-semibold">Nishyan</h2>
-                    <p className="text-sm text-gray-400 underline">Visit Store</p>
+                    <p className="text-sm text-gray-400 underline cursor-pointer">Visit Store</p>
                 </div>
                 <div className="ml-12">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="white" class="size-6">
@@ -15,7 +15,7 @@ export const Sidebar = () => {
                     </svg>
                 </div>
             </div>
-            <div className="p-2 text-sidebarText mt-3">
+            <div className="p-2 text-sidebarText mt-3 cursor-pointer">
                 <div className="flex p-1 mb-1 hover:bg-sidebarHover">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
