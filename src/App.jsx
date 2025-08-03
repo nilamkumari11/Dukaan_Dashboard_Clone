83b247bf487554d209e094d6147c6336bdb677f2
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-08-03 23:23:45 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 
 import './App.css'
+import { Navbar } from './components/Navbar'
 import { Sidebar } from './components/Sidebar'
+import { Card } from './components/Card'
+import { useState } from 'react'
 
 function App() {
-
+const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <>
-      <Sidebar></Sidebar>
-    </>
+    <div className='flex'>
+      <Sidebar isOpen={sidebarOpen}></Sidebar>
+      <div>
+        <Navbar  toggleSidebar={() => setSidebarOpen(!sidebarOpen)} ></Navbar>
+        <div className='pt-30 pl-5'>
+          <Card title={"Amount pending "} amount={"92,312.20"} orderCount={13}></Card>
+        </div>
+      </div>
+    </div>
   )
 }
 
