983a195e7e35d2d4f0ed6920701b8888d53f7838
Nilam Kumari <nilamkumari.11.rnj@gmail.com>
2025-08-04 23:57:04 +0530

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export const Navbar = ({ toggleSidebar }) => {
   return (
-    <div className="bg-white shadow-sm h-18 flex justify-between items-center px-6 fixed right-0 top-0 z-10 left-0 lg:left-55 ">
+    <div className="fixed bg-white shadow-sm h-18 flex justify-between items-center px-6 fixed right-0 top-0 z-10 left-0 lg:left-55 ">
       <button className="lg:hidden text-gray-700 mr-4" onClick={toggleSidebar}>
         <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
